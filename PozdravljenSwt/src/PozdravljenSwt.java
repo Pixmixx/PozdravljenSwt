52a34bc4d727b57f68c37be875052c4e567ec4ba
Pixie <Pixie@DESKTOP-VDTHU9P>
2020-08-05 11:21:57 +0200

    Posljemo vse datoteke v github repository

---
+++ PozdravljenSwt/src/PozdravljenSwt.java
@@ -0,0 +1,28 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * 
+ */
+
+/**
+ * @author Pixie
+ *
+ */
+public class PozdravljenSwt {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	shell.setText("Hello world!");
+	shell.open();
+	while (!shell.isDisposed()) {
+	if (!display.readAndDispatch()) display.sleep();
+	}
+	display.dispose();
+			}
+
+}
